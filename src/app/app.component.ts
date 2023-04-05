260335bcaa926f179b2465da61d919b8225a8fa1
Laboratório Deps <labdeps@lab.unesc.net>
2023-04-05 13:47:08 -0300

    primeiro commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'frontend';
+  title = 'Bem Vindo ao FrontEnd com Angular';
 }
